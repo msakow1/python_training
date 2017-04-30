77c223e102b40b14535f3496efa0bef733f05cdb
msakow1 <msakow@gmail.com>
2017-05-01 01:58:29 +0200

    General fixture created
    General fixture file created nd group, contact tests updated

---
+++ conftest.py
@@ -0,0 +1,14 @@
+
+
+import pytest
+
+
+from fixture.application import Application
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
